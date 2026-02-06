791d7a65e07f1cfaf385b964557cf1c66c8ca5a7
Trezy <tre@trezy.com>
2026-02-06 14:07:18 -0600

    feat: media section now renders screenshots

--- src/components/GamePage/Media.tsx
+++ src/components/GamePage/Media.tsx
@@ -10,54 +10,26 @@ type Props = Readonly<{ gameRecord: GameRecord }>
 export function Media(props: Props) {
 	const { gameRecord } = props
 
+	const screenshots = gameRecord?.media?.filter(
+		(item) => item.mediaType === 'screenshot',
+	)
+
 	return (
 		<section>
 			<Card className={'flex p-4'}>
 				<Header level={3}>{'Screenshots'}</Header>
 
 				<div className={'col-span-2 gap-4 grid grid-cols-3'}>
-					<AspectRatio ratio={16 / 9}>
-						<img
-							alt={`Screenshot from ${gameRecord?.name}`}
-							className={'relative h-full w-full object-cover'}
-							src={'https://placehold.co/320x180'}
-						/>
-					</AspectRatio>
-					<AspectRatio ratio={16 / 9}>
-						<img
-							alt={`Screenshot from ${gameRecord?.name}`}
-							className={'relative h-full w-full object-cover'}
-							src={'https://placehold.co/320x180'}
-						/>
-					</AspectRatio>
-					<AspectRatio ratio={16 / 9}>
-						<img
-							alt={`Screenshot from ${gameRecord?.name}`}
-							className={'relative h-full w-full object-cover'}
-							src={'https://placehold.co/320x180'}
-						/>
-					</AspectRatio>
-					<AspectRatio ratio={16 / 9}>
-						<img
-							alt={`Screenshot from ${gameRecord?.name}`}
-							className={'relative h-full w-full object-cover'}
-							src={'https://placehold.co/320x180'}
-						/>
-					</AspectRatio>
-					<AspectRatio ratio={16 / 9}>
-						<img
-							alt={`Screenshot from ${gameRecord?.name}`}
-							className={'relative h-full w-full object-cover'}
-							src={'https://placehold.co/320x180'}
-						/>
-					</AspectRatio>
-					<AspectRatio ratio={16 / 9}>
-						<img
-							alt={`Screenshot from ${gameRecord?.name}`}
-							className={'relative h-full w-full object-cover'}
-							src={'https://placehold.co/320x180'}
-						/>
-					</AspectRatio>
+					{Boolean(screenshots) &&
+						screenshots!.map((screenshot) => (
+							<AspectRatio ratio={16 / 9}>
+								<img
+									alt={`Screenshot from ${gameRecord?.name}`}
+									className={'relative h-full w-full object-cover'}
+									src={screenshot.blob?.url ?? 'https://placehold.co/320x180'}
+								/>
+							</AspectRatio>
+						))}
 				</div>
 			</Card>
 		</section>
